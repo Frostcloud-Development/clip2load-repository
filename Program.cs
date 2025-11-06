0c08f98e96ab2008a0a098515763b34d0df643d4
Avenze <alexander.pettersson@thefrostcloud.com>
2025-11-06 21:09:47 +0100

    feat(sentry): add proper dsn for sentry logging and tracing

--- Program.cs
+++ Program.cs
@@ -11,14 +11,11 @@ namespace clip2load
             // Initialize Sentry SDK for logging and analytics
             SentrySdk.Init(o =>
             {
-                o.Dsn = ""; // future me, please remember to add this
-                o.Debug = true;
+                o.Dsn = "https://566cd2389f8b8dbaebea75cf6ef12b25@o1113761.ingest.us.sentry.io/4510319876505600"; // future me, please remember to add this
                 o.TracesSampleRate = 1.0;
                 o.IsGlobalModeEnabled = true;
-                o.Release = Properties.Resources.sentry_version;
                 o.AutoSessionTracking = true;
-                o.Environment = Properties.Resources.sentry_enviroment;
-                o.StackTraceMode = Sentry.StackTraceMode.Enhanced;
+                o.Release = System.Reflection.Assembly.GetExecutingAssembly().GetName().Version?.ToString();
             });
 
             // To customize application configuration such as set high DPI settings or default font,
