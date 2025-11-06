822a1531dffd075504a0a1823aba76d451aadbbb
Avenze <alexander.pettersson@thefrostcloud.com>
2025-11-06 20:54:03 +0100

    feat: add sentry

--- Program.cs
+++ Program.cs
@@ -8,6 +8,19 @@ namespace clip2load
         [STAThread]
         static void Main()
         {
+            // Initialize Sentry SDK for logging and analytics
+            SentrySdk.Init(o =>
+            {
+                o.Dsn = ""; // future me, please remember to add this
+                o.Debug = true;
+                o.TracesSampleRate = 1.0;
+                o.IsGlobalModeEnabled = true;
+                o.Release = Properties.Resources.sentry_version;
+                o.AutoSessionTracking = true;
+                o.Environment = Properties.Resources.sentry_enviroment;
+                o.StackTraceMode = Sentry.StackTraceMode.Enhanced;
+            });
+
             // To customize application configuration such as set high DPI settings or default font,
             // see https://aka.ms/applicationconfiguration.
             ApplicationConfiguration.Initialize();
